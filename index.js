4391a8a0ffcbe1ae5a04e2cd43cbefc70d1b0f80
Maria Guadalupe Diaz <diazmariaguadalupe946@gmail.com>
2022-07-11 22:31:04 -0300

    las funcionalidades no las puedo hacer andar. Está todo hecho.

---
+++ index.js
@@ -0,0 +1,226 @@
+// SECCION HEADER
+const btnImagen = document.getElementById("btn-imagen");
+const btnTexto = document.getElementById("btn-texto");
+const ModoOscuro = document.querySelector("#btn-modo-oscuro");
+const headerModoOscuro = document.querySelector("header");
+const mainModoOscuro = document.querySelector("main");
+const asideModoOscuro = document.querySelector("aside");
+const botonesHeaderOscuro = document.getElementsByClassName("botonNav");
+
+//SECCION MAIN
+const cajaImagenMeme = document.getElementById("imagen");
+const topText = document.getElementById("top-text");
+const bottomText = document.getElementById("bottom-text");
+const descargarMeme = document.getElementById("btn-download");
+
+//SECCION FORMULARIOS
+const formularioImagen = document.getElementById("formularioImagen");
+const formularioTexto = document.getElementById("formularioTexto");
+const filtroBrillo = document.getElementById("brillo");
+const filtroOpacidad = document.getElementById("opacidad");
+const filtroContraste = document.getElementById("contraste");
+const filtroDesenfoque = document.getElementById("desenfoque");
+const filtroGrises = document.getElementById("escala-grises");
+const filtroSepia = document.getElementById("sepia");
+const filtroHue = document.getElementById("hue");
+const filtroSaturado = document.getElementById("saturado");
+const filtroNegativo = document.getElementById("negativo");
+const reestablecerFiltros = document.getElementById("btn-reestablecer");
+const colorDeFondo = document.getElementById("selectorColoresFondo");
+const cambiarColor = document.getElementById("selectorColor");
+const textSuperior = document.getElementById("superior");
+const textInferior = document.getElementById("inferior");
+const colorLetra = document.getElementById("color-letra");
+const colorFondo = document.getElementById("color-fondo");
+const fondoTransparente = document.getElementById("fondo-transparente");
+const selectorFuente = document.getElementById("selector-fuente");
+const tamañofuente = document.getElementById("tamanioFuente");
+const contornoNinguno = document.getElementById("contorno-ninguno");
+const contornoClaro = document.getElementById("contorno-claro");
+const contornoOscuro = document.getElementById("contorno-oscuro");
+const alinearCentro = document.getElementById("alinear-centro");
+const alinearDerecha = document.getElementById("alinear-derecha");
+const alinearIzquierda = document.getElementById("alinear-izquierda");
+const checkBoxSuperior = document.getElementById("checkbox-texto-superior");
+const checkBoxInferior = document.getElementById("checkbox-texto-inferior");
+const espaciado = document.getElementById("espaciado");
+const interlineado = document.getElementById("interlineado");
+
+//FUNCIONES DEL HEADER
+btnTexto.onclick = () => {
+  formularioImagen.style.display = "none";
+  formularioTexto.style.display = "block";
+};
+
+btnImagen.onclick = () => {
+  formularioImagen.style.display = "block";
+  formularioTexto.style.display = "none";
+};
+
+//FUNCION MODO CLARO Y OSCURO
+modoOscuro.onclick = () => {
+  headerModoOscuro.classList.toggle("header-modo-oscuro");
+  mainModoOscuro.classList.toggle("main-modo-oscuro");
+  asideModoOscuro.classList.toggle("aside-modo-oscuro");
+  // Recorro la colección HTML para hacer toggle de cada uno de los botones
+  for (let i = 0; i < botonesNavOscuro.length; i++) {
+    botonesNavOscuro[i].classList.toggle("boton-nav-oscuro");
+  }
+};
+
+//REVISAR
+//FUNCION DESCARGAR MEME
+descargarMeme = () => {
+  domtoimage.toBlob(document.getElementById("caja-meme")).then(function (blob) {
+    window.saveAs(blob, "meme-descargado.png");
+  });
+};
+descargaMeme.onclick = descargarMeme;
+
+//FUNCIONES DE FORMULARIO
+// Funcion URL
+const cambio = () => {
+  const inputTexto = document.getElementById("inputTexto").value;
+  const imagen = document.getElementById("imagen");
+  imagen.style.backgroundImage = `url(${inputTexto})`;
+  return false;
+};
+inputTexto.oninput = cambio;
+
+//Funcion cambiar filtros
+const cambioFiltro = () => {
+  const brillo = filtroBrillo.value;
+  const opacidad = filtroOpacidad.value;
+  const contraste = filtroContraste.value;
+  const desenfoque = filtroDesenfoque.value;
+  const escala = filtroEscalaDeGrises.value;
+  const sepia = filtroSepia.value;
+  const hue = filtroHue.value;
+  const saturado = filtroSaturado.value;
+  const negativo = filtroNegativo.value;
+  cajaImagenMeme.style.filter = `brightness(${brillo}) opacity(${opacidad}) contrast(${contraste}%) blur(${desenfoque}px) grayscale(${escala}%) sepia(${sepia}%) hue-rotate(${hue}deg) saturate(${saturado}%) invert(${negativo})`;
+};
+filtroBrillo.onchange = cambioFiltro;
+filtroOpacidad.onchange = cambioFiltro;
+filtroContraste.onchange = cambioFiltro;
+filtroDesenfoque.onchange = cambioFiltro;
+filtroEscalaDeGrises.onchange = cambioFiltro;
+filtroSepia.onchange = cambioFiltro;
+filtroHue.onchange = cambioFiltro;
+filtroSaturado.onchange = cambioFiltro;
+filtroNegativo.onchange = cambioFiltro;
+
+// Funcion restablacer filtros
+reestablecerFiltros.onclick = () => {
+  filtroBrillo.value = 1;
+  filtroOpacidad.value = 1;
+  filtroContraste.value = 100;
+  filtroDesenfoque.value = 0;
+  filtroEscalaDeGrises.value = 0;
+  filtroSepia.value = 0;
+  filtroHue.value = 0;
+  filtroSaturado.value = 100;
+  filtroNegativo.value = 0;
+  cajaImagenMeme.style.backgroundColor = "black";
+  cajaImagenMeme.style.backgroundBlendMode = "unset";
+  cambioFiltro();
+  return false;
+};
+
+//Funcion selector de color
+cambiarColorImagen = (event) => {
+  cajaImagenMeme.style.backgroundColor = event.target.value;
+};
+cambiarColor.onchange = cambiarColorImagen;
+
+//Funcion cambiar color de fondo
+cambiarColorDeFondo = (event) => {
+  cajaImagenMeme.style.backgroundBlendMode = event.target.value;
+};
+colorDeFondo.onchange = cambiarColorDeFondo;
+
+// Funciones cambiar texto
+textSuperior.oninput = () => {
+  topText.textContent = textSuperior.value;
+};
+textInferior.oninput = () => {
+  bottomText.textContent = textInferior.value;
+};
+
+// Funcion color letra
+colorLetra.oninput = () => {
+  topText.style.color = colorLetra.value;
+  bottomText.style.color = colorLetra.value;
+};
+// Funcion color fondo
+colorFondo.oninput = () => {
+  topText.style.backgroundColor = colorFondo.value;
+  bottomText.style.backgroundColor = colorFondo.value;
+};
+
+// funcion desaparecer texto superior e inferior
+// display none p superior
+const textoSuperiorNone = () => {
+  topText.classList.toggle("top-none");
+};
+checkboxSuperior.addEventListener("click", textoSuperiorNone);
+
+// display none inferior
+const textoInferiorNone = () => {
+  bottomText.classList.toggle("bottom-none");
+};
+checkboxInferior.addEventListener("click", textoInferiorNone);
+
+//Funcion cambiar fuente de texto
+cambiarTipoFuente = (event) => {
+  topTextP.style.fontFamily = event.target.value;
+  bottomTextP.style.fontFamily = event.target.value;
+};
+cambiarFuente.onchange = cambiarTipoFuente;
+//funcion cambiar tamaño de fuente
+cambiarTamaño = (event) => {
+  topTextP.style.fontSize = `${event.target.value}px`;
+  bottomTextP.style.fontSize = `${event.target.value}px`;
+};
+tamañoFuente.onclick = cambiarTamaño;
+
+// Funcion contorno
+contornoNinguno.onclick = () => {
+  topTextP.style.webkitTextStroke = "0px transparent";
+  bottomTextP.style.webkitTextStroke = "0px transparent";
+  return false;
+};
+contornoClaro.onclick = () => {
+  topTextP.style.webkitTextStroke = " 1.5px white";
+  bottomTextP.style.webkitTextStroke = "1px white";
+  return false;
+};
+contornoOscuro.onclick = () => {
+  topTextP.style.webkitTextStroke = " 1.5px black";
+  bottomTextP.style.webkitTextStroke = "1px black";
+  return false;
+};
+//Funcion alinear texto
+cambiarAlineacion = (event) => {
+  event.preventDefault();
+  topTextP.style.justifyContent = `${event.target.value}`;
+  bottomTextP.style.justifyContent = `${event.target.value}`;
+};
+alinearCentro.onclick = cambiarAlineacion;
+alinearDerecha.onclick = cambiarAlineacion;
+alinearIzquierda.onclick = cambiarAlineacion;
+
+// Funcion espaciado
+cambiarEspaciado = (event) => {
+  topTextP.style.paddingTop = `${event.target.value}px`;
+  topTextP.style.paddingBottom = `${event.target.value}px`;
+  bottomTextP.style.paddingTop = `${event.target.value}px`;
+  bottomTextP.style.paddingBottom = `${event.target.value}px`;
+};
+espaciado.onchange = cambiarEspaciado;
+//Funcion interlineado
+cambiarInterlineado = (event) => {
+  topTextP.style.lineHeight = event.target.value;
+  bottomTextP.style.lineHeight = event.target.value;
+};
+interlineado.onchange = cambiarInterlineado;
